670cab568912ab5879e23ea369c133e529d1ad8b
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-11-04 00:43:09 +0600

    initialize project, tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Gallery from "./Gallery";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
